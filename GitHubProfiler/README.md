34c9eb27a670aeb53d6620a1840ea37be6d98c41
98arun <82587103+98arun@users.noreply.github.com>
2021-11-23 17:32:28 +0530

    Added error handling, code formatting & minor bugs fixes

---
+++ GitHubProfiler/README.md
@@ -0,0 +1,11 @@
+# GitHub Profiler
+### Running Instruction
+Go to terminal and use following command
+
+```
+npm install
+npm run dev
+```
+
+then visit https://localhost:3000
+
--- GitHubProfiler/src/main.tsx
+++ GitHubProfiler/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import GitHubMain from "./GitHub/GitHubMain";
+import GitHubMain from "./Components/GitHubHome/GitHubMain";
 
 ReactDOM.render(
   <React.StrictMode>
